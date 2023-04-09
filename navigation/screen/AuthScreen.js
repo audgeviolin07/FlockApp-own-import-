c8b40436ebdf9242c4720f183ada1df510c0ba2b
Joanne Lin <joannelin@eduroamprvnat-172-16-9-167.near.illinois.edu>
2023-04-09 13:06:38 -0500

    font

--- navigation/screen/AuthScreen.js
+++ navigation/screen/AuthScreen.js
@@ -1,55 +1,10 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View, Text, Button, StyleSheet, Image } from 'react-native';
-import { FontAwesome } from 'react-native-vector-icons';
 import * as Font from 'expo-font';
 import birdFlying from './bird-flying.gif'
+import { useState, useEffect } from 'react';
 
-const AuthScreen = ({ onButtonClick }) => {
-  const handlePress = () => {
-    onButtonClick();
-  };
-
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>FLOCK</Text>
-      <Text style={styles.text}>GO ANYWHERE SAFE</Text>
-      <StatusBar style="auto" />
-      
-      <Image
-          style={styles.image}
-          source={birdFlying}
-            
-          />
-      <StatusBar style="auto" />
-      <Button title="Get Started" onPress={handlePress}/>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingBottom: 50
-  },
-  text: {
-    fontFamily: 'Pixel',
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'purple',
-  },
-  image: {
-    justifyContent: 'center',
-    width: 150, 
-    height: 150,
-    marginTop: 30,
-    marginBottom:70,
-  }
-});
 
 const loadFontsAsync = async () => {
   await Font.loadAsync({
@@ -60,6 +15,54 @@ const loadFontsAsync = async () => {
   });
 };
 
-loadFontsAsync();
+const AuthScreen = ({ onButtonClick }) => {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    loadFontsAsync().then(() => setFontsLoaded(true));
+  }, []);
+
+  const handlePress = () => {
+    onButtonClick();
+  };
+
+  if (!fontsLoaded) {
+    return <View />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>FLOCK</Text>
+      <Text style={styles.text}>GO ANYWHERE SAFE</Text>
+      <StatusBar style="auto" />
+      <Image style={styles.image} source={birdFlying} />
+      <StatusBar style="auto" />
+      <Button title="Get Started" onPress={handlePress} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingBottom: 50,
+  },
+  text: {
+    fontFamily: 'Pixel',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'purple',
+  },
+  image: {
+    justifyContent: 'center',
+    width: 150,
+    height: 150,
+    marginTop: 30,
+    marginBottom: 70,
+  },
+});
 
 export default AuthScreen;
